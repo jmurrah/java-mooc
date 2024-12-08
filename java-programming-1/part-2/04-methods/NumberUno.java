411f39795a21a5a5c468729d81d6ab803c81dc0d
jmurrah <jacobhmurrah@gmail.com>
2024-12-07 20:05:14 -0600

    Part 02-04 completed!

---
+++ java-programming-1/part-2/04-methods/NumberUno.java
@@ -0,0 +1,9 @@
+public class NumberUno {
+    public static void main(String[] args) {
+        int numberOne = numberUno();
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+}
